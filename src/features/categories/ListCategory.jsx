d84db404a692dcf323f89f0e51768d8b6e6f5f37
KingofKingssdom <duchuyd101@gmail.com>
2026-05-09 09:25:23 +0700

    update

--- src/features/categories/ListCategory.jsx
+++ src/features/categories/ListCategory.jsx
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom";
 import { GoPencil } from "react-icons/go";
 import { getCategory } from "../../services/ApiCategory";
 import { CgSortAz } from "react-icons/cg";
-import { MdFileDownload } from "react-icons/md";
+import { MdFileDownload, MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 function ListCategory() {
     const [categories, setCategories] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 5;
+    const indexOfLastItem = currentPage * pageSize;
+    const indexOfFirstItem = indexOfLastItem - pageSize;
 
     const fetchCategory = async () => {
         try {
@@ -22,7 +26,12 @@ function ListCategory() {
     useEffect(() => {
         fetchCategory()
     }, [])
-
+    const currentItems = categories.slice(indexOfFirstItem, indexOfLastItem);
+    const totalPages = Math.ceil(categories.length / pageSize);
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+    }
     return (
         <>
             <div className="container-admin">
@@ -65,7 +74,7 @@ function ListCategory() {
                                 </tr>
                             </thead>
                             <tbody>
-                                {categories.map((category) => (
+                                {currentItems.map((category) => (
                                     <tr key={category.id}>
                                         <td style={{ color: "red", fontWeight: "bolder" }}>{category.categoryCode}</td>
                                         <td>{category.categoryName}</td>
@@ -82,7 +91,57 @@ function ListCategory() {
 
                             </tbody>
                         </table>
+                        <div>
+                            <div className="container-button-change-page">
+                                <button
+                                    onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+                                    style={{
+                                        border: '1px solid rgb(205, 25, 24)',
+                                        textAlign: 'center',
+                                        borderRadius: '3px',
+                                        backgroundColor: currentPage === 1 ? 'rgb(225, 98, 98)' : 'rgb(205, 25, 24)',
+                                        color: 'white',
+                                        fontSize: '17px',
+                                        cursor: 'pointer'
+                                    }}
+                                >
+                                    <MdKeyboardArrowLeft />
+                                </button>
+
+                                {pageNumbers.map(number => (
+                                    <button
+                                        key={number}
+                                        onClick={() => setCurrentPage(number)}
+                                        style={{
+                                            backgroundColor: currentPage === number ? 'rgb(205, 25, 24)' : 'white',
+                                            fontWeight: currentPage === number ? 'bold' : 'normal',
+                                            color: currentPage == number ? 'white' : 'black',
+                                            cursor: "pointer",
+                                            border: '1px solid black'
+                                        }}
+                                    >
+                                        {number}
+                                    </button>
+                                ))}
+
+                                <button
+                                    onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+                                    style={{
+                                        border: '1px solid rgb(205, 25, 24)',
+                                        textAlign: 'center',
+                                        borderRadius: '3px',
+                                        backgroundColor: currentPage === totalPages ? 'rgb(225, 98, 98)' : 'rgb(205, 25, 24)',
+                                        color: 'white',
+                                        fontSize: '17px',
+                                        cursor: 'pointer'
+                                    }}
+                                >
+                                    <MdKeyboardArrowRight />
+                                </button>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </>
